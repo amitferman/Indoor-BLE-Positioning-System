6c6c85fc76e6e0dd675506a32289d4a55ae25dc8
Amit Ferman <amitferman@gmail.com>
2021-06-20 16:53:51 -0700

    changes

--- webpage/routes/index.js
+++ webpage/routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 const data = "{  \"data\": [    {      \"BLE_0\": 0,      \"BLE_1\": 0,      \"BLE_2\": 0,      \"BLE_3\": 0,      \"Column16\": \"example_value\"    }  ]}";
 
+/*
 fetch('http://c4585a85-96a8-4645-8b50-d557a27538df.centralus.azurecontainer.io/score', {
   method: 'POST', // or 'PUT'
   headers: {
@@ -11,15 +12,14 @@ fetch('http://c4585a85-96a8-4645-8b50-d557a27538df.centralus.azurecontainer.io/s
   body: JSON.stringify(data),
 })
 .then(response => response.json())
-.then(data => {
-
+.then(data => {*/
   /* GET home page. */
   router.get('/', function(req, res, next) {
     res.render('main', { loc: '20' });
     
   });
 
-});
+// });
 
 
 
