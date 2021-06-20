3ffa5c32426d3d4fb25664dd10627026f8021acb
Amit Ferman <amitferman@gmail.com>
2021-06-20 16:57:34 -0700

    changes

--- webpage/routes/index.js
+++ webpage/routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-const data = "{  \"data\": [    {      \"BLE_0\": 0,      \"BLE_1\": 0,      \"BLE_2\": 0,      \"BLE_3\": 0,      \"Column16\": \"example_value\"    }  ]}";
+const data = 
+{
+  "data": [
+    {
+      "BLE_0": 0,
+      "BLE_1": 0,
+      "BLE_2": 0,
+      "BLE_3": 0,
+      "Column16": "example_value"
+    }
+  ]
+};
 
-/*
+
 fetch('http://c4585a85-96a8-4645-8b50-d557a27538df.centralus.azurecontainer.io/score', {
   method: 'POST', // or 'PUT'
   headers: {
@@ -12,14 +23,14 @@ fetch('http://c4585a85-96a8-4645-8b50-d557a27538df.centralus.azurecontainer.io/s
   body: JSON.stringify(data),
 })
 .then(response => response.json())
-.then(data => {*/
+.then(res => {
   /* GET home page. */
   router.get('/', function(req, res, next) {
-    res.render('main', { loc: '20' });
+    res.render('main', { loc: res.result });
     
   });
 
-// });
+ });
 
 
 
