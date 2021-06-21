7281552d8c44e94be17a5f53db87a428c4dea1a3
Amit Ferman <amitferman@gmail.com>
2021-06-20 18:03:24 -0700

    changes

--- webpage/routes/index.js
+++ webpage/routes/index.js
@@ -2,41 +2,41 @@ var express = require('express');
 var router = express.Router();
 var fetch = require('node-fetch');
 
-fetch("https://ble-network-api.azurewebsites.net/locs")
-.then(res => res.json())
-.then(locs => {
-  const data = 
-  {
-    "data": [
-      {
-        "BLE_0": locs[0],
-        "BLE_1": locs[1],
-        "BLE_2": locs[2],
-        "BLE_3": locs[3],
-        "Column16": "example_value"
-      }
-    ]
-  };
-  fetch('http://c4585a85-96a8-4645-8b50-d557a27538df.centralus.azurecontainer.io/score', {
-    method: 'POST', // or 'PUT'
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  })
-  .then(response => response.json())
-  .then(mlRes => {
-    const mlResObj = JSON.parse(mlRes);
-    /* GET home page. */
-    router.get('/', function(req, res, next) {
-      res.render('main', { loc: mlResObj.result[0]});
-    });
-    module.exports = router;
-   });
-});
+const locs = await fetch("https://ble-network-api.azurewebsites.net/locs")
+.then(res => res.json());
+
+const data = 
+{
+  "data": [
+    {
+      "BLE_0": loc[0],
+      "BLE_1": loc[1],
+      "BLE_2": loc[2],
+      "BLE_3": loc[3],
+      "Column16": "example_value"
+    }
+  ]
+};
+
+
+fetch('http://c4585a85-96a8-4645-8b50-d557a27538df.centralus.azurecontainer.io/score', {
+  method: 'POST', // or 'PUT'
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(data),
+})
+.then(response => response.json())
+.then(mlRes => {
+  const mlResObj = JSON.parse(mlRes);
+  /* GET home page. */
+  router.get('/', function(req, res, next) {
+    res.render('main', { loc: mlResObj.result[0]});
+    
+  });
+
+ });
 
 
 
-
-
-
+module.exports = router;
