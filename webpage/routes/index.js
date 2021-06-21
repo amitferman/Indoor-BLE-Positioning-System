f95ab87ed239340e3c6bd86672f7fcd1fcdfab20
Amit Ferman <amitferman@gmail.com>
2021-06-20 18:08:23 -0700

    changes

--- webpage/routes/index.js
+++ webpage/routes/index.js
@@ -3,16 +3,16 @@ var router = express.Router();
 var fetch = require('node-fetch');
 
 const loc_stream = await fetch("https://ble-network-api.azurewebsites.net/locs")
-const loc = await loc_stream.json();
+const locs = await loc_stream.json();
 
 const data = 
 {
   "data": [
     {
-      "BLE_0": loc[0],
-      "BLE_1": loc[1],
-      "BLE_2": loc[2],
-      "BLE_3": loc[3],
+      "BLE_0": 0,
+      "BLE_1": 0,
+      "BLE_2": 0,
+      "BLE_3": 0,
       "Column16": "example_value"
     }
   ]
@@ -31,7 +31,8 @@ fetch('http://c4585a85-96a8-4645-8b50-d557a27538df.centralus.azurecontainer.io/s
   const mlResObj = JSON.parse(mlRes);
   /* GET home page. */
   router.get('/', function(req, res, next) {
-    res.render('main', { loc: mlResObj.result[0]});
+    // res.render('main', { loc: mlResObj.result[0]});
+    res.render('main', { loc: locs});
     
   });
 
