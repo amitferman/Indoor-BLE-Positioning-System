f5b7edd45880459f3eead42fc709e5c3fb2a64f2
Amit Ferman <amitferman@gmail.com>
2021-06-21 18:12:20 -0700

    c

--- webpage/app.js
+++ webpage/app.js
@@ -17,7 +17,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'public'))); // exposes /public files to browser
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
