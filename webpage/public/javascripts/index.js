2e1ca7a46509213d5ff5eb239189cadaedc13212
Amit Ferman <amitferman@gmail.com>
2021-06-21 23:12:40 -0700

    c

--- webpage/public/javascripts/index.js
+++ webpage/public/javascripts/index.js
@@ -9,3 +9,13 @@ const loctext = document.getElementById("loc-text");
 fetch("https://ble-network-api.azurewebsites.net/predicted-location")
 .then(res => res.text())
 .then(loc => loctext.innerText = loc);
+
+fetch("https://ble-network-api.azurewebsites.net/locs")
+.then(res => res.json())
+.then(l => JSON.parse(l))
+.then(locs => {
+    rssi0text.innerText = locs[0];
+    rssi1text.innerText = locs[1];
+    rssi2text.innerText = locs[2];
+    rssi3text.innerText = locs[3];
+});
